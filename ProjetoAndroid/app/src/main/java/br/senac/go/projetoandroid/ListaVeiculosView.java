74c15139515847df8d4a7a113023fb7a8c3234a1
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2019-12-05 14:43:17 -0300

    recyclerview com bugado lista veiculos

--- ProjetoAndroid/app/src/main/java/br/senac/go/projetoandroid/ListaVeiculosView.java
+++ ProjetoAndroid/app/src/main/java/br/senac/go/projetoandroid/ListaVeiculosView.java
@@ -1,33 +1,14 @@
 package br.senac.go.projetoandroid;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.os.Bundle;
 
 public class ListaVeiculosView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lista_veiculos_view);
-
-        FloatingActionButton floatingActionButton = findViewById(R.id.fb);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),TelaRegistroVeiculo.class);
-                startActivity(i);
-                Toast.makeText(getApplicationContext(), "tela de registro veiculo", Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-
+        setContentView(R.layout.activity_lista_veiculos_view);
     }
 }
